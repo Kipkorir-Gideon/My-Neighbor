3b09466adf3b17d3d56819eea4eb06aca0863659
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-15 18:37:02 +0300

    Add a registration urls.

--- neighborhood/urls.py
+++ neighborhood/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib.auth import views 
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('neighbor.urls')),
+    path('accounts/',include('registration.backends.simple.urls')),
+    path('logout/', views.LogoutView.as_view(next_page='/')),
+    path('api-token-auth/', obtain_auth_token),
 ]
