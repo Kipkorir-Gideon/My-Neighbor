8f55af9bb350097540dd1a33d7f8db363d9f7702
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-15 17:38:28 +0300

    Create profile model with its properties.

--- neighbor/models.py
+++ neighbor/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile_picture = CloudinaryField(blank=True, default = 'profile photo')
