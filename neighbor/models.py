467e177c67b41d0bac0cb5ddd64782ada9be9fdb
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-20 20:51:17 +0300

    Create neighborhood class and its properties.

--- neighbor/models.py
+++ neighbor/models.py
@@ -6,9 +6,11 @@ from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profile')
     profile_picture = CloudinaryField(blank=True, default = 'profile photo')
     bio = models.TextField()
+    email = models.EmailField()
+    neighborhood = models.ForeignKey('Neighborhood', on_delete=models.CASCADE)
 
     #Creates a profile when a user is created
     @receiver(post_save, sender = User)
@@ -29,3 +31,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return "%s profile" % self.user
+
+
+
+class Neighborhood(models.Model):
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=50)
+    people = models.IntegerField()
+    admin = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    police_info = models.IntegerField()
+    hospital_info = models.IntegerField()
