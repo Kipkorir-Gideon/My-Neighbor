93dafecbe01a4ece98bdd1892c43d06140a1ab00
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2021-12-24 21:57:25 +0300

    Add a login function and url path.

--- neighbor/views.py
+++ neighbor/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
+from django.contrib import messages
+from django.contrib.auth import login, authenticate
 from .models import *
 from .forms import *
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 @ login_required
@@ -16,9 +19,30 @@ def register(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, "Registration successful.")
             return redirect('neighborhood')
+        messages.error(request, "Unsuccessful registration. Invalid information.")
     form = RegisterForm
     return render(request, 'registration/register.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data('username')
+            password = form.cleaned_data('password')
+            user = authenticate(username = username, password = password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect('neighborhood')
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request, 'registration/login.html', {'form': form})
 
 
 @login_required
--- neighborhood/settings.py
+++ neighborhood/settings.py
@@ -18,6 +18,7 @@ from decouple import config,Csv
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -179,3 +180,13 @@ REST_FRAMEWORK = {
 }
 
 django_heroku.settings(locals())
+
+
+
+MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-success',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR: 'alert-danger',
+ }
