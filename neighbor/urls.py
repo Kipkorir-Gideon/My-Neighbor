70e66d8a6ba929751a1f52106c7bc23cae6a1948
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-10 15:41:52 +0300

    Fix url bug.

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -7,7 +7,7 @@ from . import views
 
 urlpatterns =[
     path('',views.neighborhood,name='neighborhood'),
-    path('accounts/register/', views.register, name='register'),
+    path('register/', views.register, name='register'),
     path('activate/<slug:uidb64>/<slug:token>/', views.activate_account, name='activate'),
     path('accounts/login/', views.login_request, name='login'),
     path('logout/', views.logout_request, name='logout'),
--- neighbor/views.py
+++ neighbor/views.py
@@ -6,7 +6,7 @@ from django.contrib.auth import login, authenticate, logout
 from .models import *
 from .forms import *
 from django.contrib.sites.shortcuts import get_current_site
-from django.utils.encoding import force_bytes, force_text
+from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.template.loader import render_to_string
 from .token_generator import account_activation_token
